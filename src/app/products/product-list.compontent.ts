aec1b03fd13d3c712d507db580cc4209c79df729
BootsStribling <williamstriblingenterprises@gmail.com>
2023-01-04 14:00:21 -0600

    include modular css for Product component

--- src/app/products/product-list.compontent.ts
+++ src/app/products/product-list.compontent.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
+import { IProduct } from "./product";
 
 @Component({
   selector: 'pm-products',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 
 export class ProductListComponent{ 
@@ -11,7 +13,7 @@ export class ProductListComponent{
   imageMargin = 2;
   showImage: boolean = false;
   listFilter: string = 'cart';
-  products : any[] = [
+  products : IProduct[] = [
     {
       "productId": 1,
       "productName": "Leaf Rake",
